8daeab572cfb3fedc496db5846aad402cda453cc
PritPatel1908 <kachhadiyaprit89039@gmail.com>
2024-07-07 14:54:14 +0530

    new

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://toors-com.vercel.app/'),
+    'url' => env('APP_URL', 'https://toors-com.vercel.app/'),
 
     /*
     |--------------------------------------------------------------------------
