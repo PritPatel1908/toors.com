b65a79e1f6f969b843f0dbac7463ac5b243c4471
PritPatel1908 <kachhadiyaprit89039@gmail.com>
2024-07-07 10:22:35 +0530

    new

--- resources/views/Templates/Users/user-base.blade.php
+++ resources/views/Templates/Users/user-base.blade.php
@@ -9,7 +9,7 @@
         <meta content="" name="description">
 
         <!-- Favicon -->
-        <link href="{{ asset('/assets/Users/img/Toors.png') }}" rel="icon">
+        <link href="{{ asset('assets/Users/img/Toors.png') }}" rel="icon">
 
         <!-- Google Web Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
