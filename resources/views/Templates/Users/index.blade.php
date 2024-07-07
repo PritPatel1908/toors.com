d27f7ac9fd6ab0a9613378f4c94da5654fc47f2b
PritPatel1908 <kachhadiyaprit89039@gmail.com>
2024-07-07 15:16:55 +0530

    after deoloy successfully

--- resources/views/Templates/Users/index.blade.php
+++ resources/views/Templates/Users/index.blade.php
@@ -1,7 +1,7 @@
 @php
     $currentUrl = parse_url(url()->current(), PHP_URL_SCHEME) . '://' . parse_url(url()->current(), PHP_URL_HOST) . parse_url(url()->current(), PHP_URL_PATH);
 @endphp
-@extends($currentUrl == 'http://127.0.0.1:8000' ? 'Templates.Users.user-base' : 'Templates.Users.user-base1')
+@extends($currentUrl == 'http://127.0.0.1' ? 'Templates.Users.user-base1' : 'Templates.Users.user-base')
 @section('content')
     <!-- Carousel Start -->
     <div class="container-fluid p-0 mb-5">
